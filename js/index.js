7c18cff46ecc726e1aaafaa7bcc904459c4c2b8c
kareem roqib <kareemolakunleh@gmail.com>
2023-09-07 07:52:52 +0100

    feat: add the toggle functionality

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
-function startApp() {
-  console.log("welcome to javaScript");
+const showMenu = document.querySelector('.hamburger-btn')
+const menu = document.querySelector('.primary-navigation--shown')
+const closeMenu = document.querySelector('.close-btn')
+const overlay = document.querySelector('.overlay')
+const removeActive = () => {
+  menu.classList.remove('active')
+  overlay.classList.remove('over-active')
 }
-startApp();
+showMenu.addEventListener('click', () => {
+  menu.classList.add('active')
+  overlay.classList.add('over-active')
+})
+closeMenu.addEventListener('click', removeActive)
+window.addEventListener('scroll', removeActive)
